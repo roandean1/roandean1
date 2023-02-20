6dfe54d3ce628f7f39a972bc2528497a0f82d195
roandean1 <105489465+roandean1@users.noreply.github.com>
2023-02-20 11:18:14 +0100

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,1 +1,16 @@
-console.log("Choo choo!");
+const EL_video = document.querySelector("#video");
+const EL_play = document.querySelector("#play");
+
+EL_play.addEventListener("click", () => {
+  const isPaused = EL_video.paused;
+  EL_video[isPaused ? "play" : "pause"]();
+  EL_video.classList.toggle("u-none", !isPaused);
+});
+#video {width: 300px;}
+
+/* Utility classes: */
+.u-none {display: none;}
+<button id="play">Toggle &amp; play</button><br>
+<video id="video" class="u-none">
+  <source src='http://vjs.zencdn.net/v/oceans.mp4' type='video/mp4'>
+</video>
